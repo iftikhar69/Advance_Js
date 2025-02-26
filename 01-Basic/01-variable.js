5afcf1b5d2c36922205340d053613872579f2ffb
iftikhar69 <iftikharali081144@gmail.com>
2025-02-26 14:27:42 +0500

    Create 01-variable.js

---
+++ 01-Basic/01-variable.js
@@ -0,0 +1,26 @@
+const accountId = 13123
+let accountEmail = "iftilarali@gmail.com"
+var accountPassword = "12234"
+accountCity = "gilgit"      
+
+// declearing >>undefined
+let accontstate;
+
+
+// accountId = 2   not allowed
+// accountEmail = "hec.gov"
+// accountPassword = "1233"
+accountCity = "hunza"
+console.log(accountCity)
+console.log(accountPassword)
+console.log(accountEmail)
+console.log(accountId)
+
+console.table([accountId , accountEmail , accountPassword , accountCity,accontstate])
+
+// var scope problem
+// {    
+//     // block scope and functioal scope
+// was changing
+// }
+
